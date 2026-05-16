73c99b14115aba490e79d3c0d3011146675569a9
Ashutosh kumar Singh  /master <93425340+E-techy@users.noreply.github.com>
2026-05-16 22:20:24 +0530

    docs: add comments for Socket.IO client connection [COMMIT_MESSAGE]

--- connect.js
+++ connect.js
@@ -1,5 +1,17 @@
+/**
+ * @fileoverview This script initializes a Socket.IO client and connects to a local server.
+ * It listens for the 'connect' event to confirm a successful connection.
+ */
 
-const socket=io('http://localhost:5500');
-socket.on('connect',()=>{
+// Initialize a Socket.IO client instance.
+// It attempts to connect to the Socket.IO server running at 'http://localhost:5500'.
+const socket = io('http://localhost:5500');
+
+// Set up an event listener for the 'connect' event.
+// This event is emitted by the Socket.IO client when a successful connection
+// to the server has been established.
+socket.on('connect', () => {
+    // Log a message to the console indicating that the client has successfully
+    // connected to the Socket.IO server.
     console.log('connected to server');
-})
+});
